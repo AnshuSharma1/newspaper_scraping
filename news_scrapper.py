5919c0b0262a55754d1c6bfb4788c15da5331719
Anshu Sharma <anshu.sharma@truebil.com>
2020-03-29 23:20:53 +0530

    Async event loop added

--- news_scrapper.py
+++ news_scrapper.py
@@ -1,3 +1,4 @@
+import asyncio
 import argparse
 import hashlib
 import json
@@ -9,8 +10,8 @@ import newspaper
 import redis
 
 ARTICLE_KEY = 'article:{id}'
-
 ARTICLE_SUMMARY_KEY = 'summary:{source}'
+ARTICLE_LIST = 'articles:zset:'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,16 +21,16 @@ def get_file_data(file_path):
         return f.readlines()
 
 
-def get_hash(identifier):
+async def get_hash(identifier):
     return hashlib.md5(identifier.encode('utf8')).hexdigest()
 
 
-def get_domain_name(url):
+async def get_domain_name(url):
     result = urlparse(url)
     return result.netloc
 
 
-def process_article(article):
+async def get_process_article_data(article):
     try:
         article.download()
         article.parse()
@@ -45,7 +46,7 @@ def process_article(article):
     topics = '|'.join(article.keywords)
 
     data = {
-        'id': str(get_hash(article.title + str(article.publish_date) + authors)),
+        'id': str(await get_hash(article.title + str(article.publish_date) + authors)),
         'current_date': str(datetime.now()),
         'authors': authors,
         'story_date': str(article.publish_date.date()),
@@ -53,7 +54,7 @@ def process_article(article):
         'body': article.text,
         'title': article.title,
         'url': article.url,
-        'source': get_domain_name(article.url),
+        'source': await get_domain_name(article.url),
         'category': category,
         'topics': topics,
         'summary': article.summary
@@ -62,12 +63,30 @@ def process_article(article):
     return data
 
 
-def save_json_file(article_list, today, dir_path):
-    output_path = dir_path + '/' + today + '/'
-    os.makedirs(output_path, exist_ok=True)
+async def ingest_data(redis_con, article_key, data):
+    if not redis_con.exists(article_key):
+        redis_con.hmset(article_key, mapping=data)
+        redis_con.zincrby(
+            name=ARTICLE_SUMMARY_KEY.format(source=data['source']),
+            amount=1,
+            value=data['story_date'],
+        )
+        redis_con.zadd(
+            name=ARTICLE_LIST,
+            mapping={
+                article_key: datetime.now().strftime('%s')
+            }
+        )
 
-    for article_data in article_list:
+
+def save_json_file(article_data, dir_path):
+    current_date = str(datetime.now().date())
+    story_date = article_data['story_date']
+    if story_date == current_date:
+        output_path = dir_path + '/' + current_date + '/'
+        os.makedirs(output_path, exist_ok=True)
         filename = output_path + article_data['source'] + '.json'
+
         with open(filename, 'w+') as fp:
             if not fp.read(1):
                 data = {
@@ -80,45 +99,36 @@ def save_json_file(article_list, today, dir_path):
                 json.dump(json_data, fp)
 
 
+@asyncio.coroutine
+def process_and_ingest(redis_con, article, dir_path):
+    article_json = yield from get_process_article_data(article)
+    save_json_file(article_json, dir_path)
+    article_key = ARTICLE_KEY.format(id=article_json['id'])
+    yield from ingest_data(redis_con, article_key, article_json)
+
+
 def get_redis_connection():
     return redis.Redis().from_url('redis://127.0.0.1:6379/3')
 
 
 def scrape_articles(dir_name, source_name):
-    current_date = str(datetime.now().date())
     news_list = get_file_data(os.path.join(BASE_DIR, source_name))
     redis_con = get_redis_connection()
-    stories_to_save = []
-    for link in news_list:
+    futures = []
+    for link in news_list[:1]:
         paper = newspaper.build(link.rstrip('\n'), memoize_articles=False)
-        newspaper.news_pool.set([paper], threads_per_source=2)
-        newspaper.news_pool.join()
-        for article in paper.articles:
-            article_json = process_article(article)
-            story_date = article_json['story_date']
-            if story_date == current_date:
-                stories_to_save.append(article_json)
-            article_key = ARTICLE_KEY.format(id=article_json['id'])
-            if not redis_con.exists(article_key):
-                redis_con.hmset(article_key, mapping=article_json)
-                redis_con.zincrby(
-                    name=ARTICLE_SUMMARY_KEY.format(source=article_json['source']),
-                    amount=1,
-                    value=article_json['story_date'],
-                )
-
-    save_json_file(
-        stories_to_save,
-        current_date,
-        dir_name
-    )
+        paper.download()
+        for article in paper.articles[:1]:
+            futures.append(process_and_ingest(redis_con, article, dir_name))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(futures))
 
 
 if __name__ == "__main__":
-    my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('--root_dir', action='store', required=True)
-    my_parser.add_argument('--source_list', action='store', required=True)
-    args = my_parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root_dir', action='store', required=True)
+    parser.add_argument('--source_list', action='store', required=True)
+    args = parser.parse_args()
 
     output_dir = args.root_dir
     source_file = args.source_list
