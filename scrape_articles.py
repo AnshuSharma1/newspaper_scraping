10710bceb522a7753a6dd7b4c865762525bdf631
Anshu Sharma <anshu.sharma@truebil.com>
2020-03-29 21:31:28 +0530

    Arguments parsed, file storage format changed

--- scrape_articles.py
+++ scrape_articles.py
@@ -1,3 +1,4 @@
+import argparse
 import hashlib
 import json
 import os
@@ -39,65 +40,64 @@ def process_article(article):
     if article.publish_date is None:
         article.publish_date = datetime.now()
 
+    authors = '|'.join(article.authors)
+    category = '|'.join(list(article.tags))
+    topics = '|'.join(article.keywords)
+
     data = {
-        'id': str(get_hash(article.url + article.title + str(article.publish_date))),
-        'current_date': datetime.now().strftime('%s'),
-        'authors': article.authors,
-        'story_date': article.publish_date.date().strftime('%s'),
-        'story_time': article.publish_date.time().strftime('%s'),
+        'id': str(get_hash(article.title + str(article.publish_date) + authors)),
+        'current_date': str(datetime.now()),
+        'authors': authors,
+        'story_date': str(article.publish_date.date()),
+        'story_time': str(article.publish_date.time()),
         'body': article.text,
         'title': article.title,
         'url': article.url,
         'source': get_domain_name(article.url),
-        'category': article.tags,
-        'topics': article.keywords,
+        'category': category,
+        'topics': topics,
         'summary': article.summary
     }
 
     return data
 
 
-def serialize_dict_data(d):
-    for key in d:
-        if not isinstance(d[key], (str, float, int)):
-            d[key] = json.dumps(d[key])
+def save_json_file(article_list, today, dir_path):
+    output_path = dir_path + '/' + today + '/'
+    os.makedirs(output_path, exist_ok=True)
 
-
-def save_json_file(article_data, article_path):
-    filename = article_path + article_data['source'] + '.json'
-    with open(filename, 'w+') as fp:
-        if not fp.read(1):
-            data = {
-                'results':  [article_data]
-            }
-            json.dump(data, fp)
-        else:
-            json_data = json.loads(fp)
-            json_data['results'] = json_data['results'] + [article_data]
-            json.dump(article_data, fp)
+    for article_data in article_list:
+        filename = output_path + article_data['source'] + '.json'
+        with open(filename, 'w+') as fp:
+            if not fp.read(1):
+                data = {
+                    'results':  [article_data]
+                }
+                json.dump(data, fp)
+            else:
+                json_data = json.loads(fp)
+                json_data['results'] = json_data['results'] + [article_data]
+                json.dump(json_data, fp)
 
 
 def get_redis_connection():
     return redis.Redis().from_url('redis://127.0.0.1:6379/3')
 
 
-def main():
-    current_date = datetime.now().date()
-    output_dir = 'news_articles/' + str(current_date) + '/'
-    os.makedirs(output_dir, exist_ok=True)
-    news_list = get_file_data(os.path.join(BASE_DIR, 'input.txt'))
+def scrape_articles(dir_name, source_name):
+    current_date = str(datetime.now().date())
+    news_list = get_file_data(os.path.join(BASE_DIR, source_name))
     redis_con = get_redis_connection()
+    stories_to_save = []
     for link in news_list:
         paper = newspaper.build(link.rstrip('\n'), memoize_articles=False)
-        print(paper.url)
         newspaper.news_pool.set([paper], threads_per_source=2)
         newspaper.news_pool.join()
         for article in paper.articles:
             article_json = process_article(article)
-            serialize_dict_data(article_json)
-            story_date = datetime.fromtimestamp(int(article_json['story_date'])).date()
+            story_date = article_json['story_date']
             if story_date == current_date:
-                save_json_file(article_json, output_dir)
+                stories_to_save.append(article_json)
             article_key = ARTICLE_KEY.format(id=article_json['id'])
             if not redis_con.exists(article_key):
                 redis_con.hmset(article_key, mapping=article_json)
@@ -107,6 +107,19 @@ def main():
                     value=article_json['story_date'],
                 )
 
+    save_json_file(
+        stories_to_save,
+        current_date,
+        dir_name
+    )
+
 
 if __name__ == "__main__":
-    main()
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument('--root_dir', action='store', required=True)
+    my_parser.add_argument('--source_list', action='store', required=True)
+    args = my_parser.parse_args()
+
+    output_dir = args.root_dir
+    source_file = args.source_list
+    scrape_articles(output_dir, source_file)
